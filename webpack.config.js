8d38578cf96d325e88d905ada81f4ce8c3f5737c
kaseopea <kaseopea@gmail.com>
2016-10-18 21:47:08 +0300

    webpack sass config fix

--- webpack.config.js
+++ webpack.config.js
@@ -3,25 +3,36 @@
 let path = require('path');
 let webpack = require('webpack');
 let copyWebpackPlugin = require('copy-webpack-plugin');
+let ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const SRC_PATH = '/app';
 
 module.exports = {
-  entry: {
-    main: './app/js/index.js'
-  },
-  output: {
-      library: '[name]',
-      filename: 'js/[name].js',
-      path: path.join(__dirname, 'dist'),
-  },
-  watch: true,
-  watchOptions:{
-      aggregateTimeout: 100
-  },
-  plugins:[
-    new copyWebpackPlugin([
-      { from: path.join(__dirname, 'app/index.html') }
-      ]),
-  ]
-}
+    entry: {
+        main: './app/js/index.js'
+    },
+    output: {
+        library: '[name]',
+        filename: 'js/[name].js',
+        path: path.join(__dirname, 'dist'),
+    },
+    watch: true,
+    watchOptions: {
+        aggregateTimeout: 100
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.scss$/,
+                // loaders: ['style', 'css', 'sass']
+                loader: ExtractTextPlugin.extract('css!sass')
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin('styles.css'),
+        new copyWebpackPlugin([
+            {from: path.join(__dirname, 'app/index.html')}
+        ])
+    ]
+};
